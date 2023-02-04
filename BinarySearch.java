28d12fa78fa7fe016a7b68550044a73e036ad6a6
vishalchandra736 <vishalchandra736@gmail.com>
2023-02-04 11:57:50 +0530

    Added : Search function to search the items in binary tree

--- BinarySearch.java
+++ BinarySearch.java
@@ -12,7 +12,6 @@ public class BinarySearch {
         }
     }
     public void insert(Integer data) {
-
         root = insert(root, data);
     }
     private Node insert(Node root, int data) {
@@ -42,7 +41,30 @@ public class BinarySearch {
         System.out.print(root.data + " ");
         inorder(root.right);
     }
-
+    public void search(Integer data) {
+        boolean inList = search(root, data);
+        System.out.println();
+        if(inList == true) {
+            System.out.println(data + " is present in List");
+            return;
+        }
+        System.out.println(data +" is Not present in List");
+    }
+    private boolean search(Node root, Integer data) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data == data) {
+            return true;
+        }
+        if (root.data < data) {
+            return search(root.right, data);
+        }
+        if (root.data > data) {
+            return search(root.left, data);
+        }
+        return false;
+    }
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
         binarySearch.insert(55);
@@ -53,6 +75,7 @@ public class BinarySearch {
         binarySearch.insert(99);
         binarySearch.insert(77);
         binarySearch.inorder();
+        binarySearch.search(100);
+        binarySearch.search(7);
     }
-
 }
